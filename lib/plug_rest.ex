4e702b0462f3f01e663ec6f08fa65c252472f867
Christopher Adams <christopher@fabricatorz.com>
2016-07-11 14:20:27 +0800

    Outline basic DSL

--- lib/plug_rest.ex
+++ lib/plug_rest.ex
@@ -1,2 +1,33 @@
 defmodule PlugRest do
+
+  @moduledoc """
+  A DSL to define a resource-oriented REST Plug.
+  """
+
+  @doc false
+  defmacro __using__(_) do
+    quote location: :keep do
+      use Plug.Builder
+      import PlugRest
+
+      plug :rest
+
+      defp rest(%{path_info: path_info} = conn, opts) do
+        do_match(conn, Enum.map(conn.path_info, &URI.decode/1))
+      end
+    end
+  end
+
+  defmacro resource(path, handler) do
+    add_route(path, handler)
+  end
+
+  defp add_route(path, _handler) do
+    {_vars, match} = Plug.Router.Utils.build_path_match(path)
+    quote do
+      defp do_match(conn, unquote(match)) do
+        conn |> put_resp_content_type("text/html") |> send_resp(200, "Plug REST")
+      end
+    end
+  end
 end
--- test/plug_rest_test.exs
+++ test/plug_rest_test.exs
@@ -1,8 +1,25 @@
 defmodule PlugRestTest do
   use ExUnit.Case
+  use Plug.Test
+
   doctest PlugRest
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defmodule IndexResource do
+  end
+
+  defmodule Router do
+    use PlugRest
+
+    resource "/", IndexResource
+  end
+
+  test "basic DSL is available" do
+    conn = conn(:get, "/")
+
+    conn = Router.call(conn, [])
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "Plug REST"
   end
 end
