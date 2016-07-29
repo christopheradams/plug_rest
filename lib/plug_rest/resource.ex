265722da311c3248c928f35563803ee2cc570dda
Christopher Adams <christopher@fabricatorz.com>
2016-07-29 19:07:40 +0800

    Add more documentation

--- lib/plug_rest/resource.ex
+++ lib/plug_rest/resource.ex
@@ -1,10 +1,89 @@
 defmodule PlugRest.Resource do
-  @moduledoc """
+  @moduledoc ~S"""
   Define callbacks and REST semantics for a Resource behaviour
 
   Based on Cowboy's cowboy_rest module. It operates on a Plug connection and a
   handler module which implements one or more of the optional callbacks.
 
+  For example, the route:
+
+      resource "/users/:username", MyApp.UserResource
+
+  will invoke the `init/2` function of `MyApp.UserResource` if it exists
+  and then continue executing to determine the state of the resource. By
+  default the resource must implement a `to_html` content handler which
+  returns a "text/html" representation of the resource.
+
+      defmodule MyApp.UserResource do
+
+        use PlugRest.Resource
+
+        def init(conn, _state) do
+          params = read_path_params(conn)
+          username = params["username"]
+          state = %{username: username}
+          {:ok, conn, state}
+        end
+
+        def allowed_methods(conn, state) do
+          {["GET"], conn, state}
+        end
+
+        def resource_exists(conn, %{username: username} = state)
+          # Look up user
+          state2 = %{state | name: "John Doe"}
+          {true, conn, state2}
+        end
+
+        def content_types_provided(conn, state) do
+          {[{"text/html", :to_html}], conn, state}
+        end
+
+        def to_html(conn, %{name: name} = state) do
+          {"<p>Hello, #{name}</p>", conn, state}
+        end
+      end
+
+  Each callback accepts a `%Plug.Conn{}` struct and the current state
+  of the resource, and returns a three-element tuple of the form `{value,
+  conn, state}`.
+
+  The resource callbacks are named below, along with their default
+  values. Some functions are skipped if they are undefined. Others have
+  no default value.
+
+      allowed_methods        : ["GET", "HEAD", "OPTIONS"]
+      allow_missing_post     : true
+      charsets_provided      : skip
+      content_types_accepted : none
+      content_types_provided : [{{"text", "html", %{}}, :to_html}]
+      delete_completed       : true
+      delete_resource        : false
+      expires                : :undefined
+      forbidden              : false
+      generate_etag          : :undefined
+      is_authorized          : true
+      is_conflict            : false
+      known_methods          : ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+      languages_provided     : skip
+      last_modified          : :undefined
+      malformed_request      : false
+      moved_permanently      : false
+      moved_temporarily      : false
+      multiple_choices       : false
+      options                : :ok
+      previously_existed     : false
+      resource_exists        : true
+      service_available      : true
+      uri_too_long           : false
+      valid_content_headers  : true
+      valid_entity_length    : true
+      variances              : []
+
+  You must also define the content handler callbacks that are specified
+  through `content_types_accepted/2` and `content_types_provided/2`. It is
+  conventional to name the functions after the content types that they
+  handle, such as `from_html` and `to_html`.
   """
 
   import PlugRest.Utils
--- lib/plug_rest/router.ex
+++ lib/plug_rest/router.ex
@@ -1,8 +1,30 @@
 defmodule PlugRest.Router do
-
-  @moduledoc """
+  @moduledoc ~S"""
   A DSL to supplement Plug Router with a resource-oriented routing algorithm.
+
+  It provides a macro to generate routes that dispatch to specific
+  resource handlers. For example:
+
+      defmodule MyApp.Router do
+        use PlugRest.Router
+
+        resource "/pages/:page", PageResource
+      end
+
+  The `resource/2` macro accepts a request of format `"/pages/VALUE"` and
+  dispatches it to the `PageResource` module, which must adopt the
+  `PlugRest.Resource` behaviour by implementing one or more of the callbacks
+  which describe the resource.
+
+  The macro accepts an optional initial state for the resource. For example:
+
+      resource "/pages/:page", PageResource, %{some_option: true}
+
+  Because the router builds on Plug's own Router, you can add additional
+  plugs into the pipeline. See the documentation for `Plug.Router` for
+  more information.
   """
+
 
   @doc false
   defmacro __using__(_) do
@@ -15,10 +37,25 @@ defmodule PlugRest.Router do
     end
   end
 
+
+  ## Resource
+
+  @doc """
+  Main API to define resource routes.
+
+  It accepts an expression representing the path, the name of a module
+  representing the resource, and an optional initial state.
+
+  ## Examples
+
+      resource "/pages/:page", PageResource, %{some_option: true}
+
+  """
   defmacro resource(path, handler, handler_state \\ []) do
     add_resource(path, handler, handler_state)
   end
 
+  ## Compiles the resource into a match macro from Plug.Router
   defp add_resource(path, handler, handler_state) do
     {vars, _match} = Plug.Router.Utils.build_path_match(path)
 
