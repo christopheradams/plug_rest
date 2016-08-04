3f5fb6a0af907918c20286e5f0f8e8094b76ea72
Christopher Adams <christopher@fabricatorz.com>
2016-08-04 16:33:34 +0800

    Fill out main module doc

--- lib/plug_rest.ex
+++ lib/plug_rest.ex
@@ -1,7 +1,18 @@
 defmodule PlugRest do
-
   @moduledoc """
-  A port of Cowboy's cowboy_rest module to Plug.
+  This is the documentation for the PlugRest project.
+
+  PlugRest includes an Elixir port of Cowboy's cowboy_rest module, and builds on
+  top of Plug's router to help your web application dispatch requests to resource
+  handler modules implementing REST semantics via a series of optional callbacks.
+
+  To work with PlugRest, you write a module that uses `PlugRest.Router`, and one or
+  more modules that use `PlugRest.Resource` to handle the requests.
+
+  For more information on Plug see:
+
+    * [Plug](https://hexdocs.pm/plug) - a specification and conveniences
+      for composable modules in between web applications
   """
 
 end
