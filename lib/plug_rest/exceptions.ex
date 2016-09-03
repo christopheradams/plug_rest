ca618890d8f3e840552da96b1cfbdc4111c284d0
Christopher Adams <christopher@fabricatorz.com>
2016-09-03 13:00:15 +0800

    Work around type error in exception

--- lib/plug_rest/exceptions.ex
+++ lib/plug_rest/exceptions.ex
@@ -9,6 +9,9 @@ defmodule PlugRest.ResourceError do
   defexception [:message, plug_status: 500]
 
   def exception(args) when is_list(args) do
+    # Force dialyzer to treat args as Keyword.t rather than `[{atom(),_}]`
+    args = Keyword.new(args)
+
     status = Keyword.get(args, :status, :internal_server_error)
     message = Keyword.get(args, :message, status)
     code = Plug.Conn.Status.code(status)
