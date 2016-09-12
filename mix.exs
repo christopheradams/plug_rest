5558599356172320017f039dbbb9fa6b03cc4850
Christopher Adams <christopher@fabricatorz.com>
2016-09-12 23:21:16 +0800

    Remove comments from mix and config files

--- mix.exs
+++ mix.exs
@@ -17,28 +17,15 @@ defmodule PlugRest.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:plug, :cowboy, :logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:plug, "~> 1.0"},
      {:cowboy, "~> 1.0"},
      {:ex_doc, ">= 0.0.0", only: :dev},
      {:dialyxir, "~> 0.3.5", only: [:dev]}]
-
   end
 
   defp description do
@@ -55,6 +42,4 @@ defmodule PlugRest.Mixfile do
      links: %{github: "https://github.com/christopheradams/plug_rest"}
     ]
   end
-
-
 end
