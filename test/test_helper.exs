7a34c8f9426f2ac867d47bfa4e67cf89a71946d6
Christopher Adams <christopher@fabricatorz.com>
2016-10-14 20:37:35 +0800

    Add macro to build test resources

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,22 @@
+defmodule PlugRest.TestHelper do
+  defmacro build_resource(resource, callbacks) do
+
+    functions = for {callback, value} <- callbacks do
+      quote do
+        def unquote(callback)(conn, state) do
+          {unquote(value), conn, state}
+        end
+      end
+    end
+
+    quote do
+      defmodule unquote(resource) do
+        use PlugRest.Resource
+
+        unquote(functions)
+      end
+    end
+  end
+end
+
 ExUnit.start()
