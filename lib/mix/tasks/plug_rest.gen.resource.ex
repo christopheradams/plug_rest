ffc49c58af7a677ccc21f9b8b1ae352fd005de0f
Christopher Adams <christopher@fabricatorz.com>
2017-08-13 16:43:23 +0800

    Rewrite, reformat resource gen task

--- lib/mix/tasks/plug_rest.gen.resource.ex
+++ lib/mix/tasks/plug_rest.gen.resource.ex
@@ -1,8 +1,4 @@
 defmodule Mix.Tasks.PlugRest.Gen.Resource do
-  use Mix.Task
-
-  import Mix.Generator
-
   @shortdoc "Generates a PlugRest resource"
 
   @moduledoc """
@@ -10,13 +6,13 @@ defmodule Mix.Tasks.PlugRest.Gen.Resource do
 
       mix plug_rest.gen.resource UserResource
 
-  The generated resource will contain:
+  The generator will add the following files to `lib/`:
 
-    * a resource file in lib/my_app/resources
+    * a resource file in lib/my_app/resources/user_resource.ex
 
-  The resource file path can be set with the option:
+  The resource file path can be set with the `--path` option:
 
-      mix plug_rest.gen.resource UserResource --path "web/resources"
+      mix plug_rest.gen.resource UserResource --path "lib/my_app_web/resources"
 
   In an umbrella project, run the mix task in the root of the app, or
   specify the app with:
@@ -27,8 +23,17 @@ defmodule Mix.Tasks.PlugRest.Gen.Resource do
 
       mix plug_rest.gen.resource UserResource --no-comments
   """
+  use Mix.Task
+
+  import Mix.Generator
+
   def run(args) do
-    switches = [use: :string, app: :string, path: :string]
+    switches = [
+      use: :string,
+      app: :string,
+      path: :string,
+    ]
+
     {opts, parsed, _} = OptionParser.parse(args, switches: switches)
 
     resource =
@@ -43,11 +48,11 @@ defmodule Mix.Tasks.PlugRest.Gen.Resource do
         true ->
           opts[:app]
         false  ->
-          Mix.Project.config |> Keyword.get(:app) |> Atom.to_string
+          Mix.Project.config() |> Keyword.get(:app) |> Atom.to_string()
       end
 
     if is_nil(app_lib) do
-      Mix.raise ("The app must be specified in mix.exs or with the --app switch")
+      Mix.raise "The app must be specified in mix.exs or with the --app switch"
     end
 
     apps_path =
@@ -59,30 +64,38 @@ defmodule Mix.Tasks.PlugRest.Gen.Resource do
     underscored = Macro.underscore(resource)
     app_mod = Macro.camelize(app_lib)
 
-    default_opts = [path: Path.join([apps_path, "lib", app_lib, "resources"]),
-                    use: "PlugRest.Resource", no_comments: false]
+    default_opts = [
+      path: Path.join([apps_path, "lib", app_lib, "resources"]),
+      use: "PlugRest.Resource", no_comments: false,
+    ]
 
     opts = Keyword.merge(default_opts, opts)
 
-    resource_module = Enum.join([app_mod, resource], ".")
-
     file =
-      case String.slice(opts[:path], -3..-1) == ".ex" do
-        true ->
-          opts[:path]
-        false ->
+      case Path.extname(opts[:path]) do
+        "" ->
           Path.join([opts[:path], underscored]) <> ".ex"
+        ".ex" ->
+          opts[:path]
+        _ ->
+          Mix.raise "The --path option must name a directory or .ex file"
       end
 
-    file |> Path.dirname |> create_directory
+    :ok = file |> Path.dirname() |> create_directory()
+
+    resource_module = Enum.join([app_mod, resource], ".")
+
+    template_bindings = [
+      module: resource_module,
+      comments: !opts[:no_comments],
+      resource: resource,
+      resources_use: opts[:use],
+    ]
 
     plug_app_dir = Application.app_dir(:plug_rest)
     template_path = "priv/templates/plug_rest.gen.resource/resource.ex"
     template = Path.join(plug_app_dir, template_path)
-    contents = EEx.eval_file(template, [module: resource_module,
-                                        comments: !opts[:no_comments],
-                                        resource: resource,
-                                        resources_use: opts[:use]])
+    contents = EEx.eval_file(template, template_bindings)
 
     :ok = File.write(file, contents)
 
